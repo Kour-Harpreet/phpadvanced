fe358d86785c91055ad53ccea379d8c1ea71865c
harpreet <Harpreet Kour>
2020-08-13 12:03:07 -0600

    Added API call - working!

---
+++ php-api.php
@@ -0,0 +1,16 @@
+<?php
+include 'includes/pokemon-api.php';
+global $title;
+$title = 'PHP API Example';
+include 'templates/header.php';
+?>
+
+  <h1><?php echo $title; ?></h1>
+
+  <?php include 'templates/navigation.php'; ?>
+
+  <h2>API Test</h2>
+
+
+
+<?php include 'templates/footer.php'; 
