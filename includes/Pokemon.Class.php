565d2ae1e300eba2274100ae04ce064671dc0c18
harpreet <Harpreet Kour>
2020-08-13 10:41:52 -0600

    Add 'level' property output and write methods to Pokemon class.

--- includes/Pokemon.Class.php
+++ includes/Pokemon.Class.php
@@ -1,9 +1,25 @@
-<?php
 
+<?php
 class Pokemon
 {
+  /**
+   * Set up some properties for your class.
+   * 
+   * Properties can be public, protected, or private.
+   */
   public $name = '';
-  private $health = 25;
-  private $type = 'normal';
+  public $health = 25;
+  public $type = 'normal';
   private $level = 1;
-} 
+
+  //  If a property is private, it can still be read/output if you set up a method for it.
+  public function getLevel () {
+    return $this->level;
+  }
+
+  // If a property is private, it can still be written to/updated if you set up a method for it.
+  public function levelUp () {
+    $this->level++;
+    return $this->level;
+  }
+}
--- pokemon.php
+++ pokemon.php
@@ -5,18 +5,22 @@ $title = 'PHP Classes (Pokemon)';
 include 'templates/header.php';
 ?>
 
-  <h1><?php echo $title; ?></h1>
+<h1><?php echo $title; ?></h1>
 
-  <?php include 'templates/navigation.php'; ?>
+<?php include 'templates/navigation.php'; ?>
 
-  <h2>Test Pokemon Output</h2>
+<h2>Test Pokemon Output</h2>
 
-  <?php
-    $pikachu = new Pokemon();
-    $pikachu->name = 'PikaPika';
-    echo $pikachu->name;
-    $pikachu->level += 1;
-    echo $pikachu->level;
-  ?>
+<?php
+$pikachu = new Pokemon();
+// The "name" property is public - so we can read and write from anywhere!
+$pikachu->name = 'PikaPika';
+echo $pikachu->name;
+// The "level" property is private - so we can ONLY read and write from within the class' logic.
+// $pikachu->level += 1;
+// echo $pikachu->level;
+$pikachu->levelUp();
+echo $pikachu->getLevel();
+?>
 
-<?php include 'templates/footer.php'; 
+<?php include 'templates/footer.php';
